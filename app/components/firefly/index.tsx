4e9bfd08429c43a25598dbcb94d9e8a9f77b7ef5
Marcos Evaristo <marcosh.evaristo15@gmail.com>
2025-08-15 00:46:28 -0300

    Initial page. Setting background.

---
+++ app/components/firefly/index.tsx
@@ -0,0 +1,8 @@
+import styles from './firefly.module.scss';
+
+export default function Firefly() {
+    return (
+        <div className={styles.firefly}></div>
+    )
+}
+
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import Firefly from "./components/firefly";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {Array.from({ length: 15 }, (_, i) => <Firefly key={i} />)}
         {children}
       </body>
     </html>
